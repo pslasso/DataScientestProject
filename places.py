58a42723e2c283f2fa402087985483b9ad303c4c
pslasso <paul.lasso.91@hotmail.com>
2023-05-14 21:11:15 +0200

    added status control on places.py

--- places.py
+++ places.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import csv
 
 df = pd.read_csv("/home/paul/Documents/Project/cities_data.csv")
 
@@ -9,18 +10,21 @@ lats = []
 names = []
 kinds = []
 
-for i in range(36):
+
+
+for i in range(1000):  
     lon = str(df['Lon'].iloc[i])
     lat = str(df['Lat'].iloc[i])
-    response = requests.get("https://api.opentripmap.com/0.1/en/places/radius?radius=1000&lon="+lon+"&lat="+lat+"&apikey=5ae2e3f221c38a28845f05b60b17b7c3473712177ccd23d0247a7ced")
-    data = response.json()
-    for place in data['features']:
-        cities.append(df['City'].iloc[i])
-        lons.append(place['geometry']['coordinates'][0])
-        lats.append(place['geometry']['coordinates'][1])
-        names.append(place['properties']['name'])
-        kinds.append(place['properties']['kinds'])
-        
+    response = requests.get("https://api.opentripmap.com/0.1/en/places/radius?radius=1000&lon="+lon+"&lat="+lat+"&rate=3&apikey=5ae2e3f221c38a28845f05b6611d689a2de8f6f04047db87740dc9ca")
+    if response.status_code == 200:
+        data = response.json()
+        print(response)
+        for place in data['features']:
+            cities.append(df['City'].iloc[i])
+            lons.append(place['geometry']['coordinates'][0])
+            lats.append(place['geometry']['coordinates'][1])
+            names.append(place['properties']['name'])
+            kinds.append(place['properties']['kinds'])
         
 
 
@@ -28,8 +32,15 @@ tuples = list(zip(cities, names, lons, lats, kinds))
 
 tuples
 
-places = pd.DataFrame(tuples, columns=["city", "Name", "Lon", "Lat", "Kind"]) 
+places = pd.DataFrame(tuples, columns=["City", "Name", "Lon", "Lat", "Kind"]) 
 
 print(places)
 
-places.to_csv("/home/paul/Documents/Project/places.csv")
+places.to_csv("/home/paul/Documents/DataScientestProject/places.csv")
+
+
+
+#with open('places.csv','a') as fd:
+#    writer = csv.writer(fd)
+#    for row in places:
+#        writer.writerow(row)
